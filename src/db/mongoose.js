4f8134a4367ba75ebc2dc5aa47819590b0a6dca1
samarth <samarth.gugnani30@gmail.com>
2019-11-12 02:30:01 +0530

    db changed

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,5 +1,4 @@
 const mongoose = require("mongoose");
-const mongodbdatabase = "mongodb+srv://taskapp:ajayrenu30@cluster0-makaa.mongodb.net/task-manager-api?retryWrites=true&w=majority"
 mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api',{
    useNewUrlParser:true,
    useUnifiedTopology:true,
