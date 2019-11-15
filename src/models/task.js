df590cbeaf9981a0d485a23e2f0e29cf062028cd
samarth <samarth.gugnani30@gmail.com>
2019-11-15 19:12:57 +0530

    sorting techniques added

--- src/models/task.js
+++ src/models/task.js
@@ -1,22 +1,26 @@
 const mongoose = require("mongoose");
 
-
-const task = mongoose.model("tasks",{
-    description:{
-       type:String,
-       required:true,
-       trim:true
-    },
-    completed:{
-       type:Boolean,
-       required:true,
-       trim:true
-    },
-    owner:{
-       type: mongoose.Schema.Types.ObjectId,
-       required:true,
-       ref:'User'
-    }
- })
+const taskSchema = new mongoose.Schema(
+   {
+      description:{
+         type:String,
+         required:true,
+         trim:true
+      },
+      completed:{
+         type:Boolean,
+         required:true,
+         trim:true
+      },
+      owner:{
+         type: mongoose.Schema.Types.ObjectId,
+         required:true,
+         ref:'User'
+      }
+   },{
+      timestamps:true
+   }
+)
+const task = mongoose.model("tasks",taskSchema);
  
  module.exports = task;
